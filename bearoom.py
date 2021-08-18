8279477da3be6551e22c31799d129bb50e0acf58
Yashna Dawda <yashnadawda512@gmail.com>
2021-08-19 01:47:48 +0530

    Create bearoom.py

---
+++ bearoom.py
@@ -0,0 +1,8 @@
+class bear:
+    def __init__(self):
+        self.name = "Bear"
+        self.hp = 10
+        self.damage = 2
+
+    def print():
+        print("You have made it to the Bear Room!")
