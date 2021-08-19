4c8692bb9d02945bf887fb1e664465be2a6cbc01
aradhyatalan <84051714+aradhyatalan@users.noreply.github.com>
2021-08-19 16:14:41 +0530

    added spice to starting room

--- commonstart.py
+++ commonstart.py
@@ -1,7 +1,14 @@
 import bearoom
 from bearoom import bear
 
-print("enter which room u want to go to: 1- bear room, 2 - monster room")
+print("\nGame: RUN FROM THE REAL\n")
+print("\t\tYou find yourself stuck outside the real world.")
+print("\t    You don't know if you're dreaming or if you're awake.")
+print("\tIf you die, you may wake up from the nightmare of your own brain") 
+print("\t\t\t\t\tOR") 
+print("\t\t    You might never wake up ever again")
+
+print("Enter which room u want to go to: 1- bear room, 2 - monster room")
 a=int(input())
 obj = bear()
 def choice():
